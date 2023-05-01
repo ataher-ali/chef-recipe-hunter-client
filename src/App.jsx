3654c8bef2ce9f183cd9a56f90410414c7bf59ab
ataher-ali <ataher.dev@gmail.com>
2023-05-01 23:39:21 +0600

    reactrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css'
+import { Outlet } from 'react-router-dom';
+import Header from './Pages/Components/Header/Header';
+import Footer from './Pages/Components/Footer/Footer';
 
 
 const App = () => {
   return (
     <>
-      <h2>hello ! app</h2>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,53 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Pages/Home/Home.jsx';
+import About from './Pages/About/About.jsx';
+import Blog from './Pages/Blog/Blog.jsx';
+import Login from './Pages/Account/Login.jsx';
+import Register from './Pages/Account/Register.jsx';
+import Error from './Pages/Error/Error.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[
+      {
+        path:'/',
+        element: <Home></Home>
+      },
+      {
+        path:'/*',
+        element: <Error></Error>
+      },
+      {
+        path:'/about',
+        element: <About></About>
+      },
+      {
+        path:'/blog',
+        element: <Blog></Blog>
+      },
+      {
+        path:'/login',
+        element: <Login></Login>
+      },
+      {
+        path:'/register',
+        element: <Register></Register>
+      },
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
